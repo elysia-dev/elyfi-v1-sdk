a8686f60df0f365bfa347b853224d88740048978
donguk.seo <donguk.seo@modori.io>
2022-04-08 14:37:09 +0900

    fix export statement

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export type { IncentivePool } from "./types/IncentivePool";
 export type { AssetBond } from "./types/AssetBond";
 export type { MoneyPool } from "./types/MoneyPool";
 export type { Tokenizer } from "./types/Tokenizer";
-export { StakingPoolV2 } from "./types/StakingPoolV2"
+export type { StakingPoolV2 } from "./types/StakingPoolV2"
 
 export { AssetBondFactory } from "./factories/AssetBondFactory"
 export { ERC20Factory } from "./factories/ERC20Factory"
