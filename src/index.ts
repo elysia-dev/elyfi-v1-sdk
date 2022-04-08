1c4168f0f736df649909bbba584fca8eb83a7bbb
donguk.seo <donguk.seo@modori.io>
2022-04-08 14:10:58 +0900

    add stakingpool v2

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export type { IncentivePool } from "./types/IncentivePool";
 export type { AssetBond } from "./types/AssetBond";
 export type { MoneyPool } from "./types/MoneyPool";
 export type { Tokenizer } from "./types/Tokenizer";
+export { StakingPoolV2 } from "./types/StakingPoolV2"
 
 export { AssetBondFactory } from "./factories/AssetBondFactory"
 export { ERC20Factory } from "./factories/ERC20Factory"
@@ -10,3 +11,4 @@ export { IncentivePoolFactory } from "./factories/IncentivePoolFactory"
 export { MoneyPoolFactory } from "./factories/MoneyPoolFactory"
 export { TokenizerFactory } from "./factories/TokenizerFactory"
 export { StakingPoolFactory } from "./factories/StakingPoolFactory"
+export { StakingPoolV2factory } from "./factories/StakingPoolV2factory"
